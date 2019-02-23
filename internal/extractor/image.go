4055ed79512c27b375ebc3a9136e9541f1b641c6
Tim Tosi <timothee.tosi@gmail.com>
2019-02-23 18:57:34 +0000

    fix(extractor): fix lint issue

--- internal/extractor/image.go
+++ internal/extractor/image.go
@@ -6,7 +6,7 @@ import "golang.org/x/net/html"
 // page. It uses `t` as the token to analyse and its `tokenType`. It returns
 // the link value or an empty `string` if `t` does not correspond to a link.
 func GetImg(t html.Token, tokenType html.TokenType) string {
-	if tokenType == html.StartTagToken && t.Data == "img" {
+	if t.Data == "img" && tokenType == html.StartTagToken {
 		for _, att := range t.Attr {
 			if att.Key == "src" {
 				return att.Val
