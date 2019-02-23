6a820faba00910d43cec242f19da4ce25a4e69f5
Tim Tosi <timothee.tosi@gmail.com>
2019-02-23 19:00:55 +0000

    update(cmd): plug pipes in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	crawler "github.com/timtosi/mcrawler/internal"
+	"github.com/timtosi/mcrawler/internal/domain"
+	"github.com/timtosi/mcrawler/internal/extractor"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	t := domain.NewTarget("localhost:8080") // TO UPDATE
+
+	c := crawler.NewCrawler()
+	w := crawler.NewWorker()
+	e := extractor.NewExtractor(extractor.GetImg, extractor.GetLinkBasic)
+	a := crawler.NewArchiver()
+	f, err := crawler.NewFollower(t.BaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := c.Run(t, w, e, f, a); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("shutdown")
 }
