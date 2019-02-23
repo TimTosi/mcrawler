6b3399eb7452795552c8783aa599e40aead1a0e4
Tim Tosi <timothee.tosi@gmail.com>
2019-02-23 19:00:25 +0000

    feat(internal): add Follower controlling struct

---
+++ internal/follower.go
@@ -0,0 +1,63 @@
+package crawler
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"sync"
+
+	"github.com/timtosi/mcrawler/internal/domain"
+)
+
+// Follower is a `struct` controlling that the pages crawled are only located
+// on the `originHost` host.
+type Follower struct {
+	originHost string
+}
+
+// NewFollower returns a new `*crawler.Follower` or an `error` if something
+// bad occurs.
+func NewFollower(originURL string) (*Follower, error) {
+	urlStruct, err := url.Parse(originURL)
+	if err != nil {
+		return nil, fmt.Errorf("NewFollower: %v", err)
+	} else if len(urlStruct.Host) == 0 {
+		return nil, fmt.Errorf("NewFollower: no host found in %s", originURL)
+	}
+	return &Follower{originHost: urlStruct.Host}, nil
+}
+
+// IsSameHost ensures that `link` is located on `originHost`. It returns `true`
+// if it is the case, `false` otherwise or an `error` if `link` cannot
+// be parsed properly by `url.Parse`.
+func (f *Follower) IsSameHost(link string) (bool, error) {
+	urlStruct, err := url.Parse(link)
+	if err != nil {
+		return false, fmt.Errorf("IsSameHost: %v", err)
+	} else if len(urlStruct.Host) == 0 {
+		return false, fmt.Errorf("IsSameHost: no host found in %s", link)
+	}
+
+	if urlStruct.Host != f.originHost {
+		return false, nil
+	}
+	return true, nil
+}
+
+// Pipe connects `in` and `out` together. Any `*domain.Target` received from
+// `in` will be checked against `f.originHost` and be discarded if its host does
+// not match `f.originHost`.
+//
+// NOTE: This function will loop over a channel until `in` is closed. After that
+// it will close `out`.
+func (f *Follower) Pipe(wg *sync.WaitGroup, in <-chan *domain.Target, out chan<- *domain.Target) {
+	defer close(out)
+
+	for t := range in {
+		if ok, err := f.IsSameHost(t.BaseURL); err != nil {
+			log.Printf("Follower: %f", err)
+		} else if ok {
+			out <- t
+		}
+	}
+}
