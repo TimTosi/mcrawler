dada4fa4dd05d0eaa67f06be4f62cef5aae93eb1
Tim Tosi <timothee.tosi@gmail.com>
2019-02-23 19:25:54 +0000

    update(internal): add concurrency protection to Archiver & erase unused functin

--- internal/archiver.go
+++ internal/archiver.go
@@ -9,33 +9,31 @@ import (
 // Archiver is a `struct` checking that a given URL has not already been seen.
 type Archiver struct {
 	archive map[string]bool
+	mu      *sync.Mutex
 }
 
 // NewArchiver returns a new `*crawler.Archiver`
 func NewArchiver() *Archiver {
-	return &Archiver{archive: make(map[string]bool)}
+	return &Archiver{
+		archive: make(map[string]bool),
+		mu:      &sync.Mutex{},
+	}
 }
 
 // IsAlreadySeen compares `url` to `a.archive` and returns `true` if it is
 // already seen. If not, `url` is stored in `a.archive` and the function
 // returns `false`.
+//
+// NOTE: This function is thread-safe.
 func (a *Archiver) IsAlreadySeen(url string) bool {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
 	if _, ok := a.archive[url]; ok {
 		return true
 	}
 	a.archive[url] = true
 	return false
-}
-
-// GetArchive returns a `[]string` containing every single URL contained in
-// `a.archive`.
-func (a *Archiver) GetArchive() []string {
-	var res []string
-
-	for key := range a.archive {
-		res = append(res, key)
-	}
-	return res
 }
 
 // Pipe connects `in` and `out` together. Any `t` received from `in` will
